0b8d54de7ede0e9b6fe335f738d2163036a89fd7
Chandra Prakash <scp.thedevil@gmail.com>
2021-03-01 16:36:08 +0530

    Fixed TNEB with 100% evaluation
    Co-Authored-By: Aftaab-mohammed-2712 <78999714+Aftaab-mohammed-2712@users.noreply.github.com>
    Signed-off-by: Chandra Prakash <scp.thedevil@gmail.com>

--- OOPS/session2.cpp
+++ OOPS/session2.cpp
@@ -331,3 +331,105 @@ int main()
 
     return 0;
 }
+
+//Q. 16: TNEB Billing
+
+#include <iostream>
+using namespace std;
+class Electric
+{
+    float unit;
+    char name[20];
+
+public:
+    void accept()
+    {
+
+        cin >> name;
+
+        cin >> unit;
+    }
+    void print_bill();
+};
+void Electric::print_bill()
+{
+    float bill = 0.0;
+    if (unit >= 0 && unit <= 100)
+        bill = (500 + (unit * 0.40));
+    else if (unit > 100 && unit <= 300)
+        bill = (500 + (100 * 0.40) + ((unit - 100) * 0.50));
+    else if (unit > 300)
+        bill = (500 + (100 * 0.40) + (200 * 0.50) + ((unit - 300) * 0.60));
+    if (bill > 250)
+        bill = (bill + (bill * (15 / 100)));
+    cout << "\nConsumer Name:" << name;
+    cout << "\nConsumed:" << unit;
+
+    cout << "\nBill to pay:" << bill;
+}
+
+int main()
+
+{
+
+    Electric e[10];
+
+    int i, cnt;
+
+    cin >> cnt;
+
+    cout << "\nNumber of Consumers:" << cnt;
+
+    for (i = 0; i < cnt; i++)
+
+        e[i].accept();
+
+    for (i = 0; i < cnt; i++)
+
+        e[i].print_bill();
+
+    return 0;
+}
+
+//Q. 17: Digital Library
+
+#include <iostream>
+using namespace std;
+class Library
+{
+public:
+    string name;
+    int r, bc, c = 0;
+
+    Library(int a, string s, int b)
+    {
+        name = s;
+        r = a;
+        bc = b;
+    }
+
+    void show()
+    {
+        cout << "Roll No:" << r << endl
+             << "Name of the Student:" << name << endl
+             << "Code of Book Accessed:" << bc << endl;
+    }
+
+    void count()
+    {
+        c++;
+    }
+};
+
+int main()
+{
+    int s, p, q, r;
+    string n, m;
+    cin >> s >> n >> p >> q >> m >> r;
+
+    Library lib1(s, n, p);
+    lib1.show();
+    Library lib2(q, m, r);
+    lib2.show();
+    return 0;
+}
